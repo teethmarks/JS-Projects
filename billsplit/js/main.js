7d156cf91095a0c092eac29bed42e5766f54ba29
teethmarks <teethmarks@gmail.com>
2016-12-07 00:46:10 +0800

    made it show selected number of fields

--- billsplit/js/main.js
+++ billsplit/js/main.js
@@ -1,6 +1,6 @@
 var pax;
 var plates;
-var i = 0;
+var i;
 
 $('.submit').on('click', function(e){
 	e.preventDefault();
@@ -11,14 +11,20 @@ $('.submit').on('click', function(e){
 	plates = $('#plates').val();
 
 	for (i = 0; i < pax; i++){
-		$('.namefields').append('<input type="text" class="form-control" value="' + String.fromCharCode(i + 65) + '"></div>');
+		$('.namefields').append('<input type="text" class="form-control" class="namefield" value="' + String.fromCharCode(i + 65) + '"></div>');
 	}
-
-	// $('.namefields').append('');
 });
+
+
 
 $('.next').on('click', function(){
 	$('.start').hide();
 	$('.calculator').show();
-	$('header').text('Person A')
+	createCalculator();
+
+	function createCalculator(){
+		for (a = 0; a < i; a++){
+			$('.calculator').append('<br /><input type="text" class="form-control" value=""><br /><button class="btn btn-success btn-block btn-lg next">Next</button>');
+		}
+	}
 });
