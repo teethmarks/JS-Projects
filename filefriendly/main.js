68f48f301dfe7f3877b6a9700003f3eae835de3a
teethmarks <teethmarks@gmail.com>
2016-12-10 08:02:29 +0800

    Added copy button

--- filefriendly/main.js
+++ filefriendly/main.js
@@ -4,7 +4,7 @@ function convertName() {
     converted = $('.entered').val();
     converted = converted.toLowerCase();
     converted = converted.replace(/ /g, '_');
-    $('.result').html('<div id="foo" class="result-wrap">' + converted + '</div>');
+    $('.result').html('<div id="foo" class="result-wrap">' + converted + '</div> <button data-clipboard-target="#foo" class="btn btn-lg btn-primary">COPY</button>');
 }
 
 $('.entered').on('keyup', function(e) {
@@ -13,3 +13,5 @@ $('.entered').on('keyup', function(e) {
     	$('.result-wrap').hide();
     }
 });
+
+new Clipboard('.btn');
